479c9765a13b7d3eb1bb146f31fe068543e3b049
Dina Tahoun <dinatahoun992@gmail.com>
2020-07-21 21:16:24 +0200

    connect to a local postgresql database

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ from forms import *
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
+
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
